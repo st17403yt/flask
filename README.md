d274932c5578c438c923042f51d8ef6525c3a668
Tsutsu0335 <72368680+st17403yt@users.noreply.github.com>
2021-06-26 19:09:26 +0900

    main

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 # flask
 flask todo application
 
+タスクの追加、編集、削除ができるだけ
+
+ライブラリのインストール
 pip install Flask
 pip install Flask-SQLAlchemy
 
+サーバの起動
 python main.py
+localhost 8001
 
 実行確認済み環境
 python 3.9.5
-
-参考
-# https://myafu-python.com/todo/
--- main.py
+++ main.py
@@ -10,7 +10,10 @@ db = SQLAlchemy(app)
 
 
 class Task(db.Model):
+    # テーブルの設定
     __tablename__ = "tasks"
+
+    # カラムの設定
     taskid = db.Column(db.Integer, primary_key=True, autoincrement=True)
     userid = db.Column(db.String(100))
     title = db.Column(db.String(50))
@@ -19,17 +22,20 @@ class Task(db.Model):
     place = db.Column(db.String(300))
 
 
+# データベースの生成
 db.create_all()
 
-
+# localhostにアクセスしたときの処理
 @app.route('/')
 def index():
+    # templatesのindex.htmlでタスクを全て表示
     tasks = Task.query.all()
     return render_template("index.html", tasks=tasks)
 
-
+# localhost/createにpostされた時の処理
 @app.route('/create', methods=["POST"])
 def new():
+    # 入力されたデータでタスクを追加してindexにリダイレクト
     task = Task()
     task.userid = request.form["userid"]
     task.title = request.form["title"]
@@ -40,16 +46,18 @@ def new():
     db.session.commit()
     return redirect(url_for('index'))
 
-
+# 編集ボタンが押された時の処理
 @app.route('/edit', methods=["POST"])
 def edit():
+    # 選択されたタスクの編集画面へ遷移
     taskid = request.form["taskid"]
     task = Task.query.filter_by(taskid=taskid).first()
     return render_template("edit.html", task=task)
 
-
+# 編集画面からpostされた時の処理
 @app.route('/update', methods=["POST"])
 def update():
+    # 編集後のデータを設定してindexにリダイレクト
     taskid = request.form["taskid"]
     task = Task.query.filter_by(taskid=taskid).first()
     task.title = request.form["title"]
@@ -59,9 +67,10 @@ def update():
     db.session.commit()
     return redirect(url_for('index'))
 
-
+# localhostにアクセスしたとき
 @app.route('/delete', methods=["POST"])
 def delete():
+    # 選択されたタスクを削除してindexにリダイレクト
     taskid = request.form["taskid"]
     task = Task.query.filter_by(taskid=taskid).first()
     db.session.delete(task)
@@ -70,4 +79,5 @@ def delete():
 
 
 if __name__ == "__main__":
+    # サーバの設定 address:localhost port:8001
     app.run(debug=True, host=os.getenv('APP_ADDRESS', 'localhost'), port=8001)
